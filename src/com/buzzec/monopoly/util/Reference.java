ba9a04e81ed0bcf0895a9abd4fce153aa29772c0
Buzzec <bbetter9101@gmail.com>
2017-08-18 01:04:45 -0500

    Added GO_MONEY reference.

--- src/com/buzzec/monopoly/util/Reference.java
+++ src/com/buzzec/monopoly/util/Reference.java
@@ -13,7 +13,8 @@ public class Reference {
             SIDES_ON_DIE = 6,
             MORTGAGE_DIVISOR = 2,
             BUILDING_SELL_DIVISOR = 2,
-            STARTING_MONEY = 4000;
+            STARTING_MONEY = 4000,
+            GO_MONEY = 200;
     public static final double
             MORTGAGE_BUY_BACK_FACTOR = 1.1;
     public static final Log
